c9ddc9abda7900f68c7bacb29de57b483971f94a
William Pereira <MetalHexx@outlook.com>
2022-09-03 22:41:53 -0400

    Added todos with tests

--- poc/apps/frontend/src/app/app.component.ts
+++ poc/apps/frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  title: string;
+}
+
 @Component({
   selector: 'poc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'Welcome core';
+  todos: Todo[] = [{ title: 'Todo1' }, { title: 'Todo2' }];
+
+  addTodo() {
+    this.todos.push({
+      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    });
+  }
 }
